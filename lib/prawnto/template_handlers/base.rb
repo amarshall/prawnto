64d2ade875fdd35c5ab51d351c3a89c2695f53de
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-10-08 21:13:04 -0400

    Fix for undefined method error

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -1,11 +1,11 @@
 module Prawnto
   module TemplateHandlers
     class Base < (::Rails::VERSION::MAJOR < 3 ? ::ActionView::TemplateHandler : ::Object)
-      include ::ActionView::TemplateHandlers::Compilable if ::Rails::VERSION::MAJOR < 3
-      
+      include ::ActionView::TemplateHandlers::Compilable
+
       def compile(template)
         "_prawnto_compile_setup;" +
-        "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
+        "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" +
         "#{template.source}\n" +
         "pdf.render;"
       end
